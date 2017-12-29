4bf5a45680932be85278e09754cd8865cb644629
Michał Świtoń <michal.switon@op.pl>
2017-12-29 16:09:07 +0100

    modified scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,10 @@
 var femaleNames = ["Asia", "Kasia", "Ola", "Jola"];
 var maleNames = ["Piotrek", "Marek", "Arek", "Jarek"];
 var allNames = femaleNames.concat(maleNames);
-
-console.log(allNames);
-
 var newName = "Marian";
 
 if (allNames.indexOf(newName) === -1) {
-    var pushedName = allNames.push(newName);
+    allNames.push(newName);
 };
 
-console.log(pushedName);
+console.log(allNames);
